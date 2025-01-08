f39ff0e79da8b85e0eba0356333880eb4f0617e8
jumbocarrot0 <jumbocarrot0@gmail.com>
2025-01-08 16:45:18 +1000

    Fix reloading issues with boss blind

--- mod/data/blind.lua
+++ mod/data/blind.lua
@@ -1,5 +1,12 @@
-SMODS.Atlas({ key = 'arcanum_blinds', path = 'ra_blind_atlas.png', px = 34, py = 34, frames = 21, atlas_table =
-'ANIMATION_ATLAS' })
+SMODS.Atlas({
+    key = 'arcanum_blinds',
+    path = 'ra_blind_atlas.png',
+    px = 34,
+    py = 34,
+    frames = 21,
+    atlas_table =
+    'ANIMATION_ATLAS'
+})
 
 if ReduxArcanumMod.config.new_content then
     SMODS.Blind { -- The Bane
@@ -10,34 +17,19 @@ if ReduxArcanumMod.config.new_content then
         },
         boss = { min = 2 },
 
-        drawn_to_hand = function(self)
-            sendDebugMessage("Blind Activated", "ReduxArcanumDebugLogger")
-            -- Only activate once
-            if G.GAME.current_round.hands_played == 0 and G.GAME.current_round.discards_used == 0 then
-                G.E_MANAGER:add_event(Event({
-                    blocking = false,
-                    trigger = 'after',
-                    delay = 0.1,
-                    func = function()
-                        if G.STATE == G.STATES.ROUND_EVAL then
-                            for i = 1, #G.consumeables.cards do
-                                G.consumeables.cards[i]:set_debuff(false)
-                            end
-                            return true
-                        else
-                            if not G.GAME.blind.disabled then
-                                for i = 1, #G.consumeables.cards do
-                                    G.consumeables.cards[i]:set_debuff(true)
-                                end
-                            end
-                            return false
-                        end
-                    end
-                }))
+        recalc_debuff = function(self, card, from_blind)
+            for i = 1, #G.consumeables.cards do
+                G.consumeables.cards[i]:set_debuff(true)
+                if G.consumeables.cards[i].debuff then G.consumeables.cards[i].debuffed_by_blind = true end
             end
         end,
 
         disable = function()
+            for i = 1, #G.consumeables.cards do
+                G.consumeables.cards[i]:set_debuff(false)
+            end
+        end,
+        defeat = function()
             for i = 1, #G.consumeables.cards do
                 G.consumeables.cards[i]:set_debuff(false)
             end
