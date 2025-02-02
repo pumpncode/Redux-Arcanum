d2a465d5651a43945818101558e43de3d2558437
jumbocarrot0 <jumbocarrot0@gmail.com>
2025-02-02 14:26:05 +1000

    Essence of Comedy more consistent with Constellation

--- localization/default.lua
+++ localization/default.lua
@@ -301,8 +301,9 @@ return {
             j_ReduxArcanum_essence_of_comedy = {
                 name = "Essence of Comedy",
                 text = {
-                    "Gains {X:mult,C:white} X#1# {} Mult",
-                    "per {C:alchemical}Alchemical{} card used",
+                    "Gains {X:mult,C:white} X#1# {} Mult every",
+                    "time an {C:alchemical}Alchemical{}",
+                    "card is used",
                     "{C:inactive}(Currently {X:mult,C:white} X#2# {C:inactive} Mult)"
                 }
             },
